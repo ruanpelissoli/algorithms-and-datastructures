a85763ff1ca92eefc11d5264bf215c95bdc43779
Ruan Pelissoli <ruanpelissoli@gmail.com>
2022-11-03 22:18:11 -0300

    hashing + algos

---
+++ Algorithms/CheckIfTheSentenceIsAPangram.cs
@@ -0,0 +1,24 @@
+﻿namespace AlgorithmsAndDataStructures.Algorithms;
+internal class CheckIfTheSentenceIsAPangram : IAlgorithmSolver
+{
+    public void Solve()
+    {
+        Console.WriteLine(nameof(CheckIfTheSentenceIsAPangram));
+        Console.WriteLine(Solution("thequickbrownfoxjumpsoverthelazydog"));
+        Console.WriteLine();
+    }
+
+    static bool Solution(string sentence)
+    {
+        int alphabetLength = 26;
+        var result = new HashSet<char>();
+
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            if (!result.Contains(sentence[i]))
+                result.Add(sentence[i]);
+        }
+
+        return result.Count == alphabetLength;
+    }
+}
