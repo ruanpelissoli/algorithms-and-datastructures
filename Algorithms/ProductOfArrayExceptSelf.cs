c699944790710815e4ae0fccd233757a1930bbae
Ruan Pelissoli <ruanpelissoli@gmail.com>
2022-12-27 08:54:38 -0300

    algos

---
+++ Algorithms/ProductOfArrayExceptSelf.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgorithmsAndDataStructures.Algorithms;
+
+// https://leetcode.com/problems/product-of-array-except-self/ 
+// Array
+// Prefix Sum
+internal class ProductOfArrayExceptSelf : IAlgorithmSolver
+{
+    public void Solve()
+    {
+        Console.WriteLine(nameof(ProductOfArrayExceptSelf));
+        Console.WriteLine(Solution(new int[] { 1, 2, 3, 4 }));
+        Console.WriteLine();
+    }
+
+    static int[] Solution(int[] nums)
+    {
+        var answer = new int[nums.Length];
+
+        int prefix = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int postfix = 1;
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            answer[i] *= postfix;
+            postfix *= nums[i];
+        }
+
+        return answer;
+    }
+}
---
+++ Algorithms/ValidPalindrome.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace AlgorithmsAndDataStructures.Algorithms;
+
+// https://leetcode.com/problems/valid-palindrome/
+// Two-pointer
+internal class ValidPalindrome : IAlgorithmSolver
+{
+    public void Solve()
+    {
+        Console.WriteLine(nameof(ValidPalindrome));
+        Console.WriteLine(Solution("A man, a plan, a canal: Panama"));
+        Console.WriteLine();
+    }
+
+    static bool Solution(string s)
+    {
+        if (string.IsNullOrWhiteSpace(s))
+            return true;
+
+        StringBuilder sb = new();
+
+        foreach (char c in s.ToLower())
+        {
+            if (char.IsLetterOrDigit(c))
+                sb.Append(c);
+        }
+
+        for (int i = 0; i < sb.Length / 2; i++)
+        {
+            if (sb[i] != sb[sb.Length - 1 - i])
+                return false;
+        }
+
+        return true;
+    }
+}
