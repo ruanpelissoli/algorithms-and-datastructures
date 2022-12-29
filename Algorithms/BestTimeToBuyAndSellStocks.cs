1c503a0a3e49f7de2bed09db5f091333932b33ad
Ruan Pelissoli <ruanpelissoli@gmail.com>
2022-12-29 19:18:17 -0300

    algos

---
+++ Algorithms/BestTimeToBuyAndSellStocks.cs
@@ -0,0 +1,33 @@
+﻿namespace AlgorithmsAndDataStructures.Algorithms;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// Sliding Window
+// Dynamic Programming
+internal class BestTimeToBuyAndSellStocks : IAlgorithmSolver
+{
+    public void Solve()
+    {
+        Console.WriteLine(nameof(BestTimeToBuyAndSellStocks));
+        Console.WriteLine(Solution(new int[] { 7, 1, 5, 3, 6, }));
+        Console.WriteLine(Solution(new int[] { 7, 6, 4, 3, 1, }));
+        Console.WriteLine();
+    }
+
+    static int Solution(int[] prices)
+    {
+        int ans = 0;
+        int i = 0, j = 1;
+
+        while (j < prices.Length)
+        {
+            if (prices[j] > prices[i])
+                ans = Math.Max(ans, prices[j] - prices[i]);
+            else
+                i = j;
+
+            j++;
+        }
+
+        return ans;
+    }
+}
---
+++ Algorithms/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿namespace AlgorithmsAndDataStructures.Algorithms;
+
+// https://algolab.so/courses/algorithms-and-data-structure-video-course/lectures/43998975
+internal class FizzBuzz : IAlgorithmSolver
+{
+    public void Solve()
+    {
+        Console.WriteLine(nameof(FizzBuzz));
+        Console.WriteLine(Solution(5));
+        Console.WriteLine();
+    }
+
+    static string[] Solution(int n)
+    {
+        var answer = new string[n];
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+                answer[i - 1] = "FizzBuzz";
+            else if (i % 3 == 0)
+                answer[i - 1] = "Fizz";
+            else if (i % 5 == 0)
+                answer[i - 1] = "Buzz";
+            else
+                answer[i - 1] = i.ToString();
+        }
+
+        return answer;
+    }
+}
